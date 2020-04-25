92ccfd5976e2338505f05ba67a3c6be76cf66c5a
liammcinroy <mcinroyw@gmail.com>
2020-04-25 14:47:19 -0400

    4x4 1D almost entirely working

--- ModelSim/2x2-systolic-n-body-tb.sv
+++ ModelSim/2x2-systolic-n-body-tb.sv
@@ -80,16 +80,90 @@ systolic_n_body_2x2_cell UUT(.clk(clk),
 initial begin
 
   // Initialize the variables
-  in_q_i = 2;
-  in_q_j = 1;
+  in_q_i = -2;
+  in_q_j = -1;
   in_m_i = 1;
   in_m_j = 1;
   in_p_right = 0;
   in_p_down = 0;
   clk = 1;
-  #10
-  $stop();
+  #10;
+  $stop;
+  clk = 0;
+  #5;
+  in_q_i = -2;
+  in_q_j = 1;
+  clk = 1;
+  #10;
+  $stop;
+  clk = 0;
+  #5;
+  in_q_i = -2;
+  in_q_j = 2;
+  clk = 1;
+  #10;
+  $stop;
 
 end
 
 endmodule
+
+
+// testbench for 2x2 with 4 bodies
+module acceleration_tb;
+
+reg clk;
+real q_1,
+     q_2,
+     q_3,
+     q_4,
+     m_1,
+     m_2,
+     m_3,
+     m_4,
+     a_1,
+     a_2,
+     a_3,
+     a_4;
+
+systolic_4_body_2x2 UUT(.clk(clk),
+		        .q_1(q_1),
+                        .q_2(q_2),
+                        .q_3(q_3),
+                        .q_4(q_4),
+		        .m_1(m_1),
+                        .m_2(m_2),
+                        .m_3(m_3),
+                        .m_4(m_4),
+		        .a_1(a_1),
+                        .a_2(a_2),
+                        .a_3(a_3),
+                        .a_4(a_4));
+
+initial begin
+
+  // Initialize variables
+  clk = 0;
+  q_1 = -2.0;
+  q_2 = -1.0;
+  q_3 = 1.0;
+  q_4 = 2.0;
+  m_1 = 1.0;
+  m_2 = 1.0;
+  m_3 = 1.0;
+  m_4 = 1.0;
+  a_1 = 0;
+  a_2 = 0;
+  a_3 = 0;
+  a_4 = 0;
+  #200;
+  $stop;
+
+end
+
+// always have clk taking care of sync across cells
+initial begin
+   forever #5 clk = ~clk;
+end
+
+endmodule;
--- ModelSim/2x2-systolic-n-body.sv
+++ ModelSim/2x2-systolic-n-body.sv
@@ -30,10 +30,10 @@ real f_ij;
 // When the clock cycle hits the next input, then proceed with calculations
 always @(posedge clk) begin
   denom = $sqrt((in_q_i - in_q_j) * (in_q_i - in_q_j));
-  if (denom < 1e-10) begin
+  if (denom < 1e-8) begin
     f_ij = 0;
   end else begin
-    f_ij = 6.67e-10 * in_m_i * in_m_j * (in_q_j - in_q_i) / denom / denom / denom;
+    f_ij = in_m_i * in_m_j * (in_q_j - in_q_i) / denom / denom / denom;  // Gravity causes errors, use in verlet
   end
   out_p_right <= in_p_right + f_ij;  // since 2x2, don't worry about diag
   out_p_down <= in_p_down - f_ij;
@@ -56,7 +56,8 @@ module systolic_n_body_2x2_integration(input wire clk,
 				       output real out_q_i_told,
 				       output real out_q_i_t);
 always @(posedge clk) begin
-  out_q_i_t <= 2.0 * in_q_i_t - in_q_i_told + in_dt * in_dt * in_a_t;
+  // We don't multiply by gravity until the end
+  out_q_i_t <= 2.0 * in_q_i_t - in_q_i_told + in_dt * in_dt * in_a_t * 6.67e-10;
   out_q_i_told <= in_q_i_t;
 end
 
@@ -85,54 +86,116 @@ module systolic_4_body_2x2(input wire clk,
 // Note 2: Yes, we have to do a separate one for each block and cell in
 // each execution. When we move to d > 1 then we can move to real matrices
 // (unknown whether SystemVerilog supports, I believe it does?).
-wire pr_00_00, pr_00_neg00, pr_00_10, pr_00_neg10,
-     pd_00_00, pd_00_neg00, pd_00_01, pd_00_neg01,
-     pr_01_00, pr_01_neg00, pr_01_10, pr_01_neg10,
-     pd_01_00, pd_01_neg00, pd_01_01, pd_01_neg01,
-     pr_11_00, pr_11_neg00, pr_11_10, pr_11_neg10,
-     pd_11_00, pd_11_neg00, pd_11_01, pd_11_neg01;
+real pr_00_00, pr_00_10, pr_00_11,
+     pd_00_00, pd_00_01,
+     pr_01_00, pr_01_neg00, pr_01_10, pr_01_neg10,  // use a for out
+     pd_01_00, pd_01_01, pd_01_neg01,  // use pd_11_neg for out
+     pr_11_00, pr_11_10, pr_11_neg10,  // use a for out
+     pd_11_00, pd_11_01, pd_11_neg01, pd_11_10, pd_11_11;
 
 // We also use wires for positions/masses between cells to accurately simulate
-wire q_00_00, q_00_01, q_00_10, q_00_11,
-     m_00_00, m_00_01, m_00_10, m_00_11,
-     q_01_00, q_01_01, q_01_10, q_01_11,
-     m_01_00, m_01_01, m_01_10, m_01_11,
-     q_11_00, q_11_01, q_11_10, q_11_11,
-     m_11_00, m_11_01, m_11_10, m_11_11;
+real q_00_00i, q_00_00j, q_00_01j, q_00_10i,
+     m_00_00i, m_00_00j, m_00_01j, m_00_10i,
+     q_01_00i, q_01_00j, q_01_01j, q_01_10i,
+     m_01_00i, m_01_00j, m_01_01j, m_01_10i,
+     q_11_00i, q_11_00j, q_11_01j, q_11_10i,
+     m_11_00i, m_11_00j, m_11_01j, m_11_10i;
 
 systolic_n_body_2x2_cell b_00_00(.clk(clk),
                                  .in_q_i(q_1), .in_q_j(q_1),
                                  .in_m_i(m_1), .in_m_j(m_1),
-                                 .in_p_right(pr_00_neg00), .in_p_down(pd_00_neg00),
-                                 .out_q_i(q_00_01), .out_q_j(q_00_10),
-                                 .out_m_i(m_00_01), .out_m_j(q_00_10),
+                                 .in_p_right(), .in_p_down(),
+                                 .out_q_i(q_00_00i), .out_q_j(q_00_00j),
+                                 .out_m_i(m_00_00i), .out_m_j(m_00_00j),
 			         .out_p_right(pr_00_00),
                                  .out_p_down(pd_00_00));
-// TODO fix the q, m broadcasts
 systolic_n_body_2x2_cell b_00_10(.clk(clk),
-                                 .in_q_i(q_2), .in_q_j(q_00_10),
-                                 .in_m_i(m_2), .in_m_j(m_00_10),
-                                 .in_p_right(pr_00_neg10), .in_p_down(pd_00_00),
-                                 .out_q_i(q_00_10), .out_q_j(q_00_01),
-                                 .out_m_i(m_00_10), .out_m_j(q_00_01),
+                                 .in_q_i(q_2), .in_q_j(q_00_00j),
+                                 .in_m_i(m_2), .in_m_j(m_00_00j),
+                                 .in_p_right(), .in_p_down(pd_00_00),
+                                 .out_q_i(q_00_10i), .out_q_j(),
+                                 .out_m_i(m_00_10i), .out_m_j(),
 			         .out_p_right(pr_00_10),
-                                 .out_p_down(pd_00_00));
+                                 .out_p_down());
 systolic_n_body_2x2_cell b_00_01(.clk(clk),
-                                 .in_q_i(q_00_10), .in_q_j(q_1),
-                                 .in_m_i(m_00_10), .in_m_j(m_1),
-                                 .in_p_right(pr_00_00), .in_p_down(pd_00_neg01),
-                                 .out_q_i(q_00_10), .out_q_j(q_00_01),
-                                 .out_m_i(m_00_10), .out_m_j(q_00_01),
+                                 .in_q_i(q_00_00i), .in_q_j(q_2),
+                                 .in_m_i(m_00_00i), .in_m_j(m_2),
+                                 .in_p_right(pr_00_00), .in_p_down(),
+                                 .out_q_i(), .out_q_j(q_00_01j),
+                                 .out_m_i(), .out_m_j(m_00_01j),
 			         .out_p_right(pr_01_neg00),
                                  .out_p_down(pd_00_01));
-// TODO fix the q, m inputs
 systolic_n_body_2x2_cell b_00_11(.clk(clk),
-                                 .in_q_i(q_00_10), .in_q_j(q_1),
-                                 .in_m_i(m_00_10), .in_m_j(m_1),
-                                 .in_p_right(pr_00_00), .in_p_down(pd_00_neg01),
-                                 .out_q_i(q_00_10), .out_q_j(q_00_01),
-                                 .out_m_i(m_00_10), .out_m_j(q_00_01),
+                                 .in_q_i(q_00_10i), .in_q_j(q_00_01j),
+                                 .in_m_i(m_00_10i), .in_m_j(m_00_01j),
+                                 .in_p_right(pr_00_10), .in_p_down(pd_00_01),
+                                 .out_q_i(), .out_q_j(),
+                                 .out_m_i(), .out_m_j(),
 			         .out_p_right(pr_01_neg10),
-                                 .out_p_down(pd_00_01));  // TODO no down
+                                 .out_p_down());
+systolic_n_body_2x2_cell b_01_00(.clk(clk),
+                                 .in_q_i(q_1), .in_q_j(q_3),
+                                 .in_m_i(m_1), .in_m_j(m_3),
+                                 .in_p_right(pr_01_neg00), .in_p_down(),
+                                 .out_q_i(q_01_00i), .out_q_j(q_01_00j),
+                                 .out_m_i(m_01_00i), .out_m_j(m_01_00j),
+			         .out_p_right(pr_01_00),
+                                 .out_p_down(pd_01_00));
+systolic_n_body_2x2_cell b_01_10(.clk(clk),
+                                 .in_q_i(q_2), .in_q_j(q_01_00j),
+                                 .in_m_i(m_2), .in_m_j(m_01_00j),
+                                 .in_p_right(pr_01_neg10), .in_p_down(pd_01_00),
+                                 .out_q_i(q_01_10i), .out_q_j(),
+                                 .out_m_i(m_01_10i), .out_m_j(),
+			         .out_p_right(pr_01_10),
+                                 .out_p_down(pd_11_neg00));
+systolic_n_body_2x2_cell b_01_01(.clk(clk),
+                                 .in_q_i(q_01_00i), .in_q_j(q_4),
+                                 .in_m_i(m_01_00i), .in_m_j(m_4),
+                                 .in_p_right(pr_01_00), .in_p_down(),
+                                 .out_q_i(), .out_q_j(q_01_01j),
+                                 .out_m_i(), .out_m_j(m_01_01j),
+			         .out_p_right(a_1),
+                                 .out_p_down(pd_01_01));
+systolic_n_body_2x2_cell b_01_11(.clk(clk),
+                                 .in_q_i(q_01_10i), .in_q_j(q_01_01j),
+                                 .in_m_i(m_01_10i), .in_m_j(m_01_01j),
+                                 .in_p_right(pr_01_10), .in_p_down(pd_01_01),
+                                 .out_q_i(), .out_q_j(),
+                                 .out_m_i(), .out_m_j(),
+			         .out_p_right(a_2),
+                                 .out_p_down(pd_11_neg01));
+systolic_n_body_2x2_cell b_11_00(.clk(clk),
+                                 .in_q_i(q_3), .in_q_j(q_3),
+                                 .in_m_i(m_3), .in_m_j(m_3),
+                                 .in_p_right(pd_11_neg00), .in_p_down(pd_11_neg00),  // TODO mess wiring
+                                 .out_q_i(q_11_00i), .out_q_j(q_11_00j),
+                                 .out_m_i(m_11_00i), .out_m_j(m_11_00j),
+			         .out_p_right(pr_11_00),
+                                 .out_p_down(pd_11_00));
+systolic_n_body_2x2_cell b_11_10(.clk(clk),
+                                 .in_q_i(q_4), .in_q_j(q_11_00j),
+                                 .in_m_i(m_4), .in_m_j(m_11_00j),
+                                 .in_p_right(), .in_p_down(pd_11_00),
+                                 .out_q_i(q_11_10i), .out_q_j(),
+                                 .out_m_i(m_11_10i), .out_m_j(),
+			         .out_p_right(pr_11_10),
+                                 .out_p_down());
+systolic_n_body_2x2_cell b_11_01(.clk(clk),
+                                 .in_q_i(q_11_00i), .in_q_j(q_4),
+                                 .in_m_i(m_11_00i), .in_m_j(m_4),
+                                 .in_p_right(pr_11_00), .in_p_down(pd_11_neg01),
+                                 .out_q_i(), .out_q_j(q_11_01j),
+                                 .out_m_i(), .out_m_j(m_11_01j),
+			         .out_p_right(a_3),
+                                 .out_p_down(pd_11_01));
+systolic_n_body_2x2_cell b_11_11(.clk(clk),
+                                 .in_q_i(q_11_10i), .in_q_j(q_11_01j),
+                                 .in_m_i(m_11_10i), .in_m_j(m_11_01j),
+                                 .in_p_right(pd_11_01), .in_p_down(pd_11_01),  // TODO mess wiring for result
+                                 .out_q_i(), .out_q_j(),
+                                 .out_m_i(), .out_m_j(),
+			         .out_p_right(a_4),
+                                 .out_p_down());
 
 endmodule  // end of 4 body module
