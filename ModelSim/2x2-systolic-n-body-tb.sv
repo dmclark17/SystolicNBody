28c4a49c82ab9aced20ad5045f8e14eeba3e7fad
liammcinroy <mcinroyw@gmail.com>
2020-04-24 22:50:22 -0400

    1D 2x2 systolic cell

---
+++ ModelSim/2x2-systolic-n-body-tb.sv
@@ -0,0 +1,97 @@
+// Systolic Arrays for n-body simulations
+// Developed 2020/4/24 by William McInroy, David Clark, and Aaron Jacobson
+//
+// This program simulates the implementation of the 2x2 systolic array cell
+// found in 2x2_systolic_n_body.sv
+
+
+// testbench for the numerical integration -- Confirmed
+module verlet_tb;
+
+reg clk;
+real in_q_i_told;
+real in_q_i_t;
+real in_a_t;
+real in_dt;
+
+real out_q_i_told;
+real out_q_i_t;
+
+systolic_n_body_2x2_integration UUT_Verlet(.clk(clk),
+					   .in_q_i_told(in_q_i_told),
+	                                   .in_q_i_t(in_q_i_t),
+ 			   	           .in_a_t(in_a_t),
+	       				   .in_dt(in_dt),
+	       				   .out_q_i_told(out_q_i_told),
+	                                   .out_q_i_t(out_q_i_t));
+
+// Create a test
+initial begin
+
+  // Initialize inputs
+  in_q_i_told = 1.5;
+  in_q_i_t = 4.0;
+  in_a_t = 1.0;
+  in_dt = 0.1;
+  clk = 1;
+  #100;
+  $display(out_q_i_told);
+  $display(out_q_i_t);
+  $stop;
+
+end
+
+endmodule
+
+
+// testbench for a single systolic cell
+module cell_tb;
+
+reg clk;
+reg reset;
+real in_q_i;
+real in_q_j;
+real in_m_i;
+real in_m_j;
+real in_p_right;
+real in_p_down;
+
+real out_q_i;
+real out_q_j;
+real out_m_i;
+real out_m_j;
+real out_p_right;
+real out_p_left;
+
+systolic_n_body_2x2_cell UUT(.clk(clk),
+		             .reset(reset),
+                             .in_q_i(in_q_i),
+			     .in_q_j(in_q_j),
+                             .in_m_i(in_m_i),
+			     .in_m_j(in_m_j),
+                             .in_p_right(in_p_right),
+			     .in_p_down(in_p_down),
+                             .out_q_i(out_q_i),
+			     .out_q_j(out_q_j),
+                             .out_m_i(out_m_i),
+			     .out_m_j(out_m_j),
+			     .out_p_right(out_p_right),
+			     .out_p_down(out_p_down));
+
+// Create a test
+initial begin
+
+  // Initialize the variables
+  in_q_i = 2;
+  in_q_j = 1;
+  in_m_i = 1;
+  in_m_j = 1;
+  in_p_right = 0;
+  in_p_down = 0;
+  clk = 1;
+  #10
+  $stop();
+
+end
+
+endmodule
--- ModelSim/2x2-systolic-n-body.sv
+++ ModelSim/2x2-systolic-n-body.sv
@@ -27,9 +27,20 @@ module systolic_n_body_2x2_cell(input wire clk,
 				output real out_p_right,
 				output real out_p_down);
 
+real denom;
+real f_ij;
+
 // When the clock cycle hits the next input, then proceed with calculations
-// always @(posedge clk) begin
-// end
+always @(posedge clk) begin
+  denom = $sqrt((in_q_i - in_q_j) * (in_q_i - in_q_j));
+  f_ij = 6.67e-10 * in_m_i * in_m_j * (in_q_j - in_q_i) / denom / denom / denom;
+  out_p_right <= in_p_right + f_ij;  // since 2x2, don't worry about diag
+  out_p_down <= in_p_down - f_ij;
+  out_q_i <= in_q_i;
+  out_m_j <= in_q_j;
+  out_m_i <= in_m_i;
+  out_m_j <= in_m_j;
+end
 
 endmodule  // end of single systolic cell module
 
