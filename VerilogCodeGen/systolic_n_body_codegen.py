290bbf907505a36c40abb5bd342d03298b437735
liammcinroy <mcinroyw@gmail.com>
2020-11-06 11:31:21 -0500

    starting the test bench autogen code

--- VerilogCodeGen/systolic_n_body_codegen.py
+++ VerilogCodeGen/systolic_n_body_codegen.py
@@ -12,14 +12,18 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description='A script to automatically generate SystemVerilog code '
                     'for n-body simulations with systolic arrays.')
-    parser.add_argument('file', type=str,
-                        help='The path pointing to the output .sv file.')
+    parser.add_argument('design_file', type=str,
+                        help='Path pointing to the output design .sv file.')
+    parser.add_argument('tb_file', type=str,
+                        help='Path pointing to the output testbench .sv file.')
     parser.add_argument('N', type=int,
                         help='The size of the systolic array.')
+    parser.add_argument('n', type=int,
+                        help='The number of bodies for testbench file.')
     return parser.parse_args()
 
 
-def generate_code(N):
+def generate_design_code(N):
     """A function to generate the SystemVerilog code for a systolic n-body.
 
     TODO: change from real to some synthesizable other form.
@@ -86,7 +90,6 @@ def generate_code(N):
              'endmodule  // end of single systolic cell module\n\n\n')
 
     # Now we add the design for the NxN array on the chip (assume it fits)
-    # TODO: make code pretty with indentations, shorter line
 
     # the module definition
     s = '\n' + (' ' * len('module systolic_{0}x{0}_3D('.format(N)))
@@ -112,22 +115,28 @@ def generate_code(N):
 
     # local wires: accumulations across/downwards, positions, masses
     s = '\n' + (' ' * len('  real '))
-    code += ('\n  real ' +
+    code += ('\n  // The accumulation across to the right wires (i, j)\n'
+             '\n  real ' +
              s.join(['pr_{0}_{1}[3],'.format(i, j)
                      for i in range(N) for j in range(N - 1)])[:-1] + ';')
-    code += ('\n  real ' +
+    code += ('\n\n  // The accumulation downwards wires (i, j)\n'
+             '\n  real ' +
              s.join(['pd_{0}_{1}[3],'.format(i, j)
                      for i in range(N - 1) for j in range(N)])[:-1] + ';')
-    code += ('\n  real ' +
+    code += ('\n\n  // The position passing wires to the right out of (i, j)\n'
+             '\n  real ' +
              s.join(['q_{0}_{1}_i[3],'.format(i, j)
                      for i in range(N) for j in range(N - 1)])[:-1] + ';')
-    code += ('\n  real ' +
+    code += ('\n\n  // The position passing wires downwards out of (i, j)\n'
+             '\n  real ' +
              s.join(['q_{0}_{1}_j[3],'.format(i, j)
                      for i in range(N - 1) for j in range(N)])[:-1] + ';')
-    code += ('\n  real ' +
+    code += ('\n\n  // The mass passing wires to the right out of (i, j)\n'
+             '\n  real ' +
              s.join(['m_{0}_{1}_i[3],'.format(i, j)
                      for i in range(N) for j in range(N - 1)])[:-1] + ';')
-    code += ('\n  real ' +
+    code += ('\n\n  // The mass passing wires downwards out of (i, j)\n'
+             '\n  real ' +
              s.join(['m_{0}_{1}_j[3],'.format(i, j)
                      for i in range(N - 1) for j in range(N)])[:-1] + ';')
 
@@ -186,12 +195,213 @@ def generate_code(N):
     return code
 
 
+def generate_testbench_code(N, n):
+    """Code to generate a testbench of the systolic design for n bodies.
+
+    Arguments:
+        N: The size of the systolic array.
+        n: The number of bodies in the simulation.
+            Must be divisible by N to ease our autogeneration.
+    """
+    if n % N != 0:
+        raise ValueError('N ({}) must divide n ({})'.format(N, n))
+
+    # start with the header
+    code = ('// Systolic array for n-body simulations. Generated code.\n'
+            '//\n'
+            '// This program implements a testbench of a {0}x{0} '.format(N) +
+            'systolic array for n-body simulations.\n\n\n')
+
+    # add the cell test bench code
+    code += ('// testbench for a single systolic cell -- Confirmed\n'
+             'module NxN_cell_tb;\n\n'
+             'reg clk;\n'
+             'real in_q_i[3];\n'
+             'real in_q_j[3];\n'
+             'real in_m_i;\n'
+             'real in_m_j;\n'
+             'real in_p_right[3];\n'
+             'real in_p_down[3];\n\n'
+             'real out_q_i[3];\n'
+             'real out_q_j[3];\n'
+             'real out_m_i;\n'
+             'real out_m_j;\n'
+             'real out_p_right[3];\n'
+             'real out_p_down[3];\n\n'
+             'systolic_n_body_3D_cell UUT(.clk(clk),\n'
+                            .in_q_i(in_q_i),\n'
+			    .in_q_j(in_q_j),\n'
+                            .in_m_i(in_m_i),\n'
+			    .in_m_j(in_m_j),\n'
+                            .in_p_right(in_p_right),\n'
+			    .in_p_down(in_p_down),\n'
+                            .out_q_i(out_q_i),\n'
+			    .out_q_j(out_q_j),\n'
+                            .out_m_i(out_m_i),\n'
+			    .out_m_j(out_m_j),\n'
+			    .out_p_right(out_p_right),\n'
+			    .out_p_down(out_p_down));\n\n'
+             '// Create a test\n'
+             'initial begin\n\n'
+             '  // Initialize the variables\n'
+             '  in_q_i[0] = -3;\n'
+             '  in_q_i[1] = -2;\n'
+             '  in_q_i[2] = -2;\n'
+             '  in_q_j[0] = -1;\n'
+             '  in_q_j[1] = -1;\n'
+             '  in_q_j[2] = 0;\n'
+             '  in_m_i = 1;\n'
+             '  in_m_j = 1;\n'
+             '  in_p_right[0] = 0;\n'
+             '  in_p_right[1] = 0;\n'
+             '  in_p_right[2] = 0;\n'
+             '  in_p_down[0] = 0;\n'
+             '  in_p_down[1] = 0;\n'
+             '  in_p_down[2] = 0;\n'
+             '  clk = 1;\n'
+             '  #5;\n'
+             '  $stop;\n'
+             '  clk = 0;\n'
+             '  #5;\n'
+             '  in_q_i[0] = -2;\n'
+             '  in_q_i[1] = -2;\n'
+             '  in_q_i[2] = 0;\n'
+             '  in_q_j[0] = 2;\n'
+             '  in_q_j[1] = 2;\n'
+             '  in_q_j[2] = 2;\n'
+             '  clk = 1;\n'
+             '  #5;\n'
+             '  $stop;\n'
+             'end\n\nendmodule\n\n\n')
+
+    # add the code for the NxN acceleration with n bodies test
+    code += ('// testbench for {0}x{0} acceleration with {1} bodies '
+             '-- Confirmed\nmodule acceleration_3D_tb;\n\n'
+             .format(N, n)
+
+    # add the variables used in the software
+    s = '\n' + (' ' * len('real '))
+    code += ('// Variables for the software side'
+             '\nreal ' +
+             s.join(['q_{}[3],'.format(i)
+                     for i in range(n)]) +
+             s.join(['a_{}[3],'.format(i)
+                     for i in range(n)]) +
+             s.join(['m_{},'.format(i)
+                     for i in range(n)])[:-1] + ';')
+
+    # add the variables to pass to UUT
+    s = '\n' + (' ' * len('real '))
+    code += ('\n\n// Variables to pass to the UUT'
+             '\nreg clk;'
+             '\nreal ' +
+             s.join(['Q_{0}i[3],'.format(i)
+                     for i in range(N)]) +
+             s.join(['Q_{0}j[3],'.format(i)
+                     for i in range(N)]) +
+             s.join(['M_{0}i,'.format(i)
+                     for i in range(N)]) +
+             s.join(['M_{0}j,'.format(i)
+                     for i in range(N)]) +
+             s.join(['PR_{0}[3],'.format(i)
+                     for i in range(N)]) +
+             s.join(['PD_{0}[3],'.format(i)
+                     for i in range(N)]) +
+             s.join(['OPR_{0}[3],'.format(i)
+                     for i in range(N)]) +
+             s.join(['OPD_{0}[3],'.format(i)
+                     for i in range(N)])[:-1] + ';')
+
+    # create the UUT
+    s = '\n' + (' ' * len('systolic_{0}x{0}_3D UUT('.format(N)))
+    code += ('\n\nsystolic_{0}x{0}_3D UUT(.clk(clk),'.format(N) + s +
+             s.join(['.q_{0}i(Q_{0}i),'.format(i)  # input row pos
+                     for i in range(N)]) + s +
+             s.join(['.q_{0}j(Q_{0}j),'.format(i)  # input col pos
+                     for i in range(N)]) + s +
+             s.join(['.m_{0}i(M_{0}i),'.format(i)  # input row mass
+                     for i in range(N)]) + s +
+             s.join(['.m_{0}j(M_{0}j),'.format(i)  # input col mass
+                     for i in range(N)]) + s +
+             s.join(['.pd_{0}(PD_{0}),'.format(i)  # input down acc
+                     for i in range(N)]) + s +
+             s.join(['.pr_{0}(PR_{0}),'.format(i)  # input right acc
+                     for i in range(N)]) + s +
+             s.join(['.out_pd_{0}(OPD_{0}),'.format(i)  # out down
+                     for i in range(N)]) + s +
+             s.join(['.out_pr_{0}(OPR_{0}),'.format(i)  # out right
+                     for i in range(N)])[:-1] + ');\n')
+
+    # initialize the variables
+    s = '\n' + (' ' * 2)
+    code = ('\ninitial begin\n' + s + 'clk = 0;' + s +
+            s.join(['q_{0}[0] = {1}; q_{0}[1] = {2}; q_{0}[2] = {3};'
+                    .format(i, 0, 0, 0)  # TODO put in initial positions
+                    for i in range(n)]) + s +
+            s.join(['a_{0}[0] = {1}; a_{0}[1] = {2}; a_{0}[2] = {3};'
+                    .format(i, 0, 0, 0)  # TODO put in initial accelerations
+                    for i in range(n)]) + s +
+            s.join(['m_{0} = {1};'
+                    .format(i, 1)  # TODO put in masses
+                    for i in range(n)]) + '\n' + s +
+            s.join(['Q_{0}i[0] = 0; Q_{0}i[1] = 0; Q_{0}i[2] = 0;'
+                    .format(i)
+                    for i in range(N)]) + s +
+            s.join(['Q_{0}j[0] = 0; Q_{0}j[1] = 0; Q_{0}j[2] = 0;'
+                    .format(i)
+                    for i in range(N)]) + s +
+            s.join(['M_{0}i = 0;'
+                    .format(i)
+                    for i in range(N)]) + s +
+            s.join(['M_{0}j = 0;'
+                    .format(i)
+                    for i in range(N)]) + s +
+            s.join(['PD_{0}[0] = 0; PD_{0}[1] = 0; PD_{0}[2] = 0;'
+                    .format(i)
+                    for i in range(N)]) + s +
+            s.join(['PR_{0}[0] = 0; PR_{0}[1] = 0; PR_{0}[2] = 0;'
+                    .format(i)
+                    for i in range(N)]) + s +
+            s.join(['OPD_{0}[0] = 0; OPD_{0}[1] = 0; OPD_{0}[2] = 0;'
+                    .format(i)
+                    for i in range(N)]) + s +
+            s.join(['OPR_{0}[0] = 0; OPR_{0}[1] = 0; OPR_{0}[2] = 0;'
+                    .format(i)
+                    for i in range(N)]))
+
+    # Now compute the timesteps block-by-block
+    # To generate the code, we take note that for block i, j
+    #   (where 0 <= i <= j < n / N) then
+    #           Q_{u}i = q_{u + i * N}
+    #           Q_{v}j = q_{v + j * N}
+    #           M_{u}i = m_{u + i * N}
+    #           M_{v}j = m_{v + j * N}
+    #           and zero in the other terms when applicable
+    #
+    # And then for the accumulations at the end of the blocks that
+    #   are off the diagonal (which output N steps after input):
+    #           a_{u + i * N} += OPR_{u}
+    #           a_{v + j * N} += OPD_{v}
+
+    # end the code for the NxN acceleration with n bodies test
+    code += ('end\n\n'
+             '// always have clk taking care of sync across cells\n'
+             'initial begin\n'
+             '  forever #5 clk = ~clk;]\n'
+             'end\n\n'
+             'endmodule')
+
+
 def main():
     args = parse_args()
 
-    code = generate_code(args.N)
-    with open(args.file, 'w') as f:
-        f.write(code)
+    design_code = generate_design_code(args.N)
+    with open(args.design_file, 'w') as f:
+        f.write(design_code)
+
+    testbench_code = generate_testbench_code(args.N, args.n)
+    with open(args.tb_file, 'w') as f:
+        f.write(testbench_code)
 
 
 if __name__ == '__main__':
